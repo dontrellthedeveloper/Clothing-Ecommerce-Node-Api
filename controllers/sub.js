81372bd7c821c467644ff2c1757fa833f4d0940b
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-01-27 16:12:03 -0800

    added CRUD operations for sub categories

---
+++ controllers/sub.js
@@ -0,0 +1,44 @@
+const Sub = require('../models/sub');
+const slugify = require('slugify');
+
+exports.create = async (req, res) => {
+    try {
+        const {name} = req.body;
+        const subCategory = await new Sub({name, slug: slugify(name)}).save();
+        res.json(subCategory);
+    } catch (err) {
+        res.status(400).send('Create sub category failed');
+    }
+};
+
+exports.list = async (req, res) => {
+    res.json(await Sub.find({}).sort({createdAt: -1}).exec());
+};
+
+exports.read = async (req, res) => {
+    let sub = await Sub.findOne({}).sort({createdAt: -1}).exec();
+    res.json(sub);
+};
+
+exports.update = async (req, res) => {
+    const {name} = req.body;
+    try {
+        const updated = await Sub.findOneAndUpdate(
+            {slug: req.params.slug},
+            {name, slug: slugify(name)},
+            {name: true}
+        );
+        res.json(updated)
+    } catch (e) {
+        res.status(400).send("Sub category update failed")
+    }
+};
+
+exports.remove = async (req, res) => {
+    try {
+        const deleted = await Sub.findOneAndDelete({slug: req.params.slug});
+        res.json(deleted);
+    } catch (e) {
+        res.status(400).send("Sub category delete failed")
+    }
+};
