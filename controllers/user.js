df767304a93457a46509cb839c2d406d2e35af81
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-02-25 21:11:40 -0800

    added the ability to empty cart

--- controllers/user.js
+++ controllers/user.js
@@ -52,3 +52,24 @@ exports.userCart = async (req, res) => {
     console.log("new cart ----> ", newCart);
     res.json({ ok: true });
 };
+
+
+exports.getUserCart = async (req, res) => {
+    const user = await User.findOne({ email: req.user.email }).exec();
+
+    let cart = await Cart.findOne({ orderdBy: user._id })
+        .populate("products.product", "_id title price totalAfterDiscount")
+        .exec();
+
+    const { products, cartTotal, totalAfterDiscount } = cart;
+    res.json({ products, cartTotal, totalAfterDiscount });
+};
+
+
+exports.emptyCart = async (req, res) => {
+    console.log("empty cart");
+    const user = await User.findOne({ email: req.user.email }).exec();
+
+    const cart = await Cart.findOneAndRemove({ orderdBy: user._id }).exec();
+    res.json(cart);
+};
--- routes/user.js
+++ routes/user.js
@@ -6,11 +6,15 @@ const router = express.Router();
 const { authCheck } = require("../middlewares/auth");
 
 const {
-    userCart
+    userCart,
+    getUserCart,
+    emptyCart
 } = require("../controllers/user");
 
 
 router.post("/user/cart", authCheck, userCart);
+router.get("/user/cart", authCheck, getUserCart); // get cart
+router.delete("/user/cart", authCheck, emptyCart); // empty cart
 
 // router.get("/user", (req, res) => {
 //     res.json({
