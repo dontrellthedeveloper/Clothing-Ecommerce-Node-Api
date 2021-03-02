9c9f42ab396ceb4a4fe9f967a92e821b3e7036e5
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-03-02 13:14:57 -0800

    changed port again. Heroku has random port in production

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ readdirSync('./routes')
 
 
 // port
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 7000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
