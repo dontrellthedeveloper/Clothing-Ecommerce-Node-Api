16589e2236f4a8089b2612333e1b582953639423
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-03-02 12:30:05 -0800

    changed port settings

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ readdirSync('./routes')
 
 
 // port
-const port = process.env.PORT = 8000;
+const port = process.env.PORT || 6000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
